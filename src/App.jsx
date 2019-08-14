933d4fcd11daca27821d7deb4390c7d3917a53b7
heenathaseen <heenathaseen98@gmail.com>
2019-08-14 14:34:53 +0530

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import './app.css';
+
+const Red = () => (
+  <div className="red">
+    <p>RED</p>
+  </div>
+);
+
+const Green = () => (
+  <div className="green">
+    <p>green</p>
+  </div>
+);
+
+const Blue = () => (
+  <div className="blue">
+    <p>blue</p>
+  </div>
+);
 
 
-const App = () => (<p>hello</p>);
+const App = () => (
+  <div className="app">
+    <p>hello</p>
+    <Red />
+    <Green />
+    <Blue />
 
+  </div>
+
+);
 export default App;
