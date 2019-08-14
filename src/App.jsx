a20ba595fcf3b75f998b3da75433cabe810e59f9
heenathaseen <heenathaseen98@gmail.com>
2019-08-14 15:06:07 +0530

    reducing  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
 import './app.css';
 
-const Red = ({ title }) => (
+const Color = ({ title }) => (
   <div
-    className="red"
-    style={{ background: title }}
-  >
-    <p>{title}</p>
-  </div>
-);
-
-const Green = ({ title }) => (
-  <div
-    className="green"
-    style={{ background: title }}
-  >
-    <p>{title}</p>
-  </div>
-);
-
-const Blue = ({ title }) => (
-  <div
-    className="blue"
+    className="color"
     style={{ background: title }}
   >
     <p>{title}</p>
@@ -32,9 +14,9 @@ const Blue = ({ title }) => (
 const App = () => (
   <div className="app">
     <p>hello</p>
-    <Red title="red" />
-    <Green title="green" />
-    <Blue title="blue" />
+    <Color title="red" />
+    <Color title="green" />
+    <Color title="blue" />
 
   </div>
 
